b957e70e841dcedbfdfb96ec04cd5878f33befd9
Chris Soncrant <christopher.soncrant@gmail.com>
2022-06-23 12:17:18 -0700

    added blog card builder component

--- public/js/components/blog-card-builder.js
+++ public/js/components/blog-card-builder.js
@@ -1,11 +1,7 @@
-const blogCardsDiv = document.querySelector(".blog-cards-section");
-const cardContainer = createElement('div', 'blog-card');
-const cardContents = createElement('div', 'blog-card-description')
-const cardImage = createElement('img', 'blog-image');
-const cardTitle = createElement('h2', null);
-const cardDescription = createElement('p', null);
-const cardReadButton = createElement('a', ['btn-1', 'read-btn'])
+import { db } from "../firebase.js"
+import { doc, getDoc, getDocs, collection } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-firestore.js";
 
+const blogCardsDiv = document.querySelector(".blog-cards-section");
 
 // console.log(cardReadButton);
 
@@ -14,7 +10,6 @@ function createElement(type, className) {
     if (className) {
         if (Array.isArray(className)) {
             className.forEach(name => {
-                console.log('name', name)
                 el.classList.add(name);
             })
         } else {
@@ -24,6 +19,41 @@ function createElement(type, className) {
     return el;
 }
 
-function createCard(src, alt, title, desc, href) {
-    blogCardsDiv
+export function createCard(obj) {
+    const cardContainer = createElement('div', 'blog-card');
+    const cardContents = createElement('div', 'blog-card-description')
+    const cardImage = createElement('img', 'card-image');
+    const cardTitle = createElement('h2', null);
+    const cardDescription = createElement('p', null);
+    const cardReadButton = createElement('a', ['btn-1', 'read-btn'])
+    
+    let {bannerImage, title, description, href} = obj;
+
+    cardImage.setAttribute('src', bannerImage);
+
+    cardTitle.textContent = title;
+
+    cardDescription.textContent = description;
+
+    cardContents.append(cardImage, cardTitle, cardDescription);
+
+    cardReadButton.setAttribute('href', href);
+    cardReadButton.textContent = 'Read';
+
+    cardContainer.append(cardContents, cardReadButton);
+
+    blogCardsDiv.appendChild(cardContainer);
 }
+
+async function getCollections() {
+    let querySnapshot = await getDocs(collection(db, 'blogs'));
+    
+    querySnapshot.forEach(doc => {
+        let obj = doc.data();
+        obj.href = doc.id;
+        console.log(obj);
+        createCard(obj);
+    })
+}
+
+getCollections();
--- public/js/home.js
+++ public/js/home.js
@@ -1,13 +1,1 @@
-import { db } from "./firebase.js"
-import { doc, getDoc, getDocs, collection } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-firestore.js";
 
-
-async function getCollections() {
-    let querySnapshot = await getDocs(collection(db, 'blogs'));
-    console.log(querySnapshot);
-    querySnapshot.forEach(doc => {
-        console.log(doc.id, '=>', doc.data());
-    })
-}
-
-// getCollections();
